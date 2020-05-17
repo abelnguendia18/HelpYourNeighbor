954ce23d653946d76069617570078894393bef33
Abel Nguendia Tapondjou <inf3417@hs-worms.de>
2020-05-17 20:03:59 +0200

    Login and signIn implemented

--- app/src/main/java/com/example/helpyourneighbor/HomePageActivity.kt
+++ app/src/main/java/com/example/helpyourneighbor/HomePageActivity.kt
@@ -2,11 +2,60 @@ package com.example.helpyourneighbor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_home_page.*
 
 class HomePageActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        //the first fragment is going to be default loaded
+        loadFragment(HomeFragment())
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener@{ item->
+        when(item.itemId)
+        {
+            R.id.home -> {
+                loadFragment(HomeFragment())
+                //Toast.makeText(this, getText(R.string.developer_name), Toast.LENGTH_LONG).show()
+                return@onNavigationItemSelectedListener true
+            }
+
+            R.id.search -> {
+                //Toast.makeText(this@MainActivity,getText(R.findViewB), Toast.LENGTH_LONG).show()
+                //Toast.makeText(this, "Search Helper", Toast.LENGTH_LONG).show()
+                loadFragment(SearchHelperFragment())
+                return@onNavigationItemSelectedListener true
+            }
+            R.id.chat ->{
+
+                //Toast.makeText(this, "Chats", Toast.LENGTH_LONG).show()
+                loadFragment(ChatFragment())
+                return@onNavigationItemSelectedListener true
+            }
+
+            R.id.my_account ->{
+
+                //Toast.makeText(this, "My Account", Toast.LENGTH_LONG).show()
+                loadFragment(AccountParameterFragment())
+                return@onNavigationItemSelectedListener true
+            }
+        }
+        return@onNavigationItemSelectedListener false
+
+    }
+    private fun loadFragment(fragment : Fragment){
+        supportFragmentManager.beginTransaction().also { fragmentTransaction ->
+            fragmentTransaction.replace(R.id.fragment_container, fragment)
+            fragmentTransaction.commit()
+        }
     }
 }
